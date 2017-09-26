aa65e4ad2f464be94a39ae088dff4d549a06429d
Andrés Pajarón <apajaron@gmail.com>
2017-09-26 11:43:42 +0200

    Add history-api-fallback to production server

--- build/server.js
+++ build/server.js
@@ -2,6 +2,8 @@ var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
 app = express()
+// handle fallback for HTML5 history API
+app.use(require('connect-history-api-fallback')())
 app.use(serveStatic(path.join(__dirname, '..', 'dist')))
 var port = process.env.PORT || 5000
 app.listen(port)
